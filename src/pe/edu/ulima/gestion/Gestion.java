ac722995e973dd5ba9017daa5fae15b27e05c3a0
Administrator <Administrator@W4213.lab.aca>
2017-01-24 20:50:36 -0500

    Lista de animales ahora tiene cabacera la cuál no debe ser leida

--- src/pe/edu/ulima/gestion/Gestion.java
+++ src/pe/edu/ulima/gestion/Gestion.java
@@ -13,23 +13,27 @@ public class Gestion {
     public void cargar(String ruta){
         Scanner sc = null;
         File f = new File(ruta);
+        int contador = 0;
         
         try {
             sc = new Scanner(f);
             while(sc.hasNext()){
                 String linea = sc.nextLine();
-                String[] datos = linea.split("::");
-                int id = Integer.parseInt(datos[0]); 
-                String nombre = datos[1];
-                String nombreCientifico = datos[2];
-                int aniosVida = Integer.parseInt(datos[3]);
-                float pesoKg = Float.parseFloat(datos[4]);
-                String habitad = datos[5];
-                String locomocion = datos[6];
-                String alimentacion = datos[7];
+                if(contador != 0){
+                    String[] datos = linea.split("::");
+                    int id = Integer.parseInt(datos[0]); 
+                    String nombre = datos[1];
+                    String nombreCientifico = datos[2];
+                    int aniosVida = Integer.parseInt(datos[3]);
+                    float pesoKg = Float.parseFloat(datos[4]);
+                    String habitad = datos[5];
+                    String locomocion = datos[6];
+                    String alimentacion = datos[7];
 
-                Animal a = new Animal(id, nombre, nombreCientifico, aniosVida, pesoKg, habitad, locomocion, alimentacion);
-                animalitos.add(a);
+                    Animal a = new Animal(id, nombre, nombreCientifico, aniosVida, pesoKg, habitad, locomocion, alimentacion);
+                    animalitos.add(a);
+                }
+                contador = contador + 1;
             }
         } catch (FileNotFoundException ex) {
             Logger.getLogger("El archivo no existe");
